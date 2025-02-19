72822c326dba30931ee8cecb79b37debc97cef7c
specialrobotdog <74635020+specialrobotdog@users.noreply.github.com>
2025-02-19 15:31:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // OpenWeatherMap API (Replace with your actual API key)
-const WEATHER_API_KEY = "0e3442cf34d7c4281dd2aab37f3fa310";
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${WEATHER_API_KEY}&units=metric`;
 
 async function isItRaining() {
